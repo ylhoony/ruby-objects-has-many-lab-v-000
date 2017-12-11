d8b98c20cae93e3f4330dbde5a6408830113726b
Hoon Lee <yl.hoony@gmail.com>
2017-12-11 17:58:19 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,14 +15,14 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    @@song_count =+ 1
+    @@song_count += 1
   end
 
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     @songs << song
     song.artist = self
-    @@song_count =+ 1
+    @@song_count += 1
   end
 
   def self.song_count
