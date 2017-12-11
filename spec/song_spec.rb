cc26a97275bd226b64bbed99ed6c6545849a1cbe
Hoon Lee <yl.hoony@gmail.com>
2017-12-11 17:59:21 +0000

    Done.

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -1,36 +1,36 @@
 require "spec_helper"
 
-describe "Song" do 
+describe "Song" do
 
   let!(:song) {Song.new("Survivor")}
-  describe "#new" do 
-    it "is initialized with an argument of a name" do 
+  describe "#new" do
+    it "is initialized with an argument of a name" do
       expect{Song.new("Say my Name")}.to_not raise_error
     end
   end
 
-  describe "#name" do 
+  describe "#name" do
     it "has a name" do
       expect(song.name).to eq("Survivor")
     end
   end
 
   describe "#artist" do
-    it "belongs to an artist" do 
+    it "belongs to an artist" do
       drake = Artist.new("Drake")
       song.artist = drake
       expect(song.artist).to eq(drake)
-    end 
+    end
   end
 
   describe "#artist_name" do
-    it "knows the name of its artist" do 
+    it "knows the name of its artist" do
       drake = Artist.new("Drake")
       song.artist = drake
       expect(song.artist_name).to eq("Drake")
-    end 
+    end
 
-    it "returns nil if the song does not have an artist" do 
+    it "returns nil if the song does not have an artist" do
       song = Song.new("Cool for the Summer")
       expect(song.artist_name).to eq nil
     end
