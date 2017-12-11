c5f4d2d160c59ab3c5ef0c577f89d6f1ae4b73ed
Hoon Lee <yl.hoony@gmail.com>
2017-12-11 17:58:47 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,12 +15,14 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
 
   def post_count
