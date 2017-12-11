515f102b712b8855ed68e6413384a906578e45cb
Hoon Lee <yl.hoony@gmail.com>
2017-12-11 17:22:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,7 @@ class Song
   end
 
   def artist_name
-    self.artist.name == "undefined" ? self.artist.name = nil : self.artist.name
+    self.artist.name == undefined ? self.artist.name = nil : self.artist.name
   end
 
 end
